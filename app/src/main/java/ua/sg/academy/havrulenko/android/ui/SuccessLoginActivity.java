51ca89357eaad136383e4df4f68ba6b94aadec1c
Alexsandr Havrulenko <havrulenko7@gmail.com>
2017-05-17 13:10:48 +0300

    add save session

--- app/src/main/java/ua/sg/academy/havrulenko/android/ui/SuccessLoginActivity.java
+++ app/src/main/java/ua/sg/academy/havrulenko/android/ui/SuccessLoginActivity.java
@@ -1,13 +1,17 @@
 package ua.sg.academy.havrulenko.android.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.TextView;
 
 import ua.sg.academy.havrulenko.android.R;
+
+import static ua.sg.academy.havrulenko.android.ui.MainActivity.KEY_SESSION_EMAIL;
 
 public class SuccessLoginActivity extends AppCompatActivity {
 
@@ -17,7 +21,7 @@ public class SuccessLoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_success_login);
         TextView textViewEmail = (TextView) findViewById(R.id.textViewUserEmail);
         AppCompatButton buttonLogout = (AppCompatButton) findViewById(R.id.buttonLogout);
-        String email = getIntent().getStringExtra("email");
+        String email = getIntent().getStringExtra(KEY_SESSION_EMAIL);
         textViewEmail.setText(getResources().getString(R.string.welcome_user_email, email));
         buttonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,8 +32,17 @@ public class SuccessLoginActivity extends AppCompatActivity {
     }
 
     private void onClickLogout() {
+        clearSession();
         Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
+
+    private void clearSession() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY_SESSION_EMAIL, "");
+        editor.apply();
+    }
+
 }
